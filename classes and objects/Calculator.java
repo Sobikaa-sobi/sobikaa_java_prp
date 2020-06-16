57237e892c8057841fb7e80aba24d8fbe7f527ce
Sobikaa-sobi <66825553+Sobikaa-sobi@users.noreply.github.com>
2020-06-16 20:45:52 +0530

    Add files via upload

---
+++ classes and objects/Calculator.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+public class Calculator {
+	static  double powerInt(int num1,int num2) {
+		return Math.pow(num1, num2);
+	}
+	static double powerDouble(double num1,int num2) {
+		return Math.pow(num1,num2);
+	}
+}
