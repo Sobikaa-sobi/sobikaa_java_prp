a6d8b5bd2372289b44bc64afcbfffe7e49b9d75d
Sobikaa-sobi <66825553+Sobikaa-sobi@users.noreply.github.com>
2020-06-16 20:44:49 +0530

    Delete CalculatorMain.java

--- Classes_and_Objects/classes and objects/CalculatorMain.java
+++
@@ -1,9 +0,0 @@
-
-public class CalculatorMain {
-
-	public static void main(String[] args) {
-		System.out.println(Calculator.powerInt(7, 3));
-		System.out.println(Calculator.powerDouble(12.5, 2));
-	}
-
-}
