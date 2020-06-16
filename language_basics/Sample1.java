e620c5280ad58e7bf2f523f93b009a2140679dd3
Sobikaa-sobi <66825553+Sobikaa-sobi@users.noreply.github.com>
2020-06-16 14:35:52 +0530

    Add files via upload

---
+++ language_basics/Sample1.java
@@ -0,0 +1,7 @@
+public class Sample1{
+      public static void main(String args[]){
+	String company=args[0];
+	String location=args[1];
+	System.out.println(company+" "+"Technologies"+" "+location);
+	}
+}
